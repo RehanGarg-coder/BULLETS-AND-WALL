7f76c4c291e43a2b31df209fe372555e3765a977
RehanGarg-coder <74549659+RehanGarg-coder@users.noreply.github.com>
2021-06-30 14:05:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var bullet,wall;
+var thickness,speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+bullet =  createSprite(50,200,50,50);
+bullet.shapeColor = color(255);
+ thickness = random(22,83);
+ speed= random(223,321);
+ weight= random(30,52);
+ bullet.velocityX =speed;
+ wall = createSprite(1200,200,thickness,height/2)
+ wall.shapeColor = color(230,230,230);
+//bullet.collide(wall);
+}
+function draw() {
+  background("lightblue");  
+
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+    
+    if(damage>10){
+      wall.shapeColor = color(255,0,0);
+    }
+    if(damage<10){
+      wall.shapeColor(0,255,0);
+    }
+    }
+  drawSprites();
+}
+function hasCollided(lbullet, lwall) 
+{ bulletRightEdge=lbullet.x +lbullet.width;
+   wallLeftEdge=lwall.x; 
+  if (bulletRightEdge>=wallLeftEdge) 
+  { return true } 
+  return false; 
+}
